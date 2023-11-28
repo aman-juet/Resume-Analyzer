31a48ef96915a416965d5ecd458d822723ccda68
Aman Kumar <114340764+aman-juet@users.noreply.github.com>
2023-11-28 23:11:46 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,7 @@ import spacy
 import en_core_web_sm
 nltk.download('stopwords')
 spacy.load('en_core_web_sm')
-from flask_sqlalchemy import SQLAlchemy
+
 import pandas as pd
 import base64, random
 import time, datetime
